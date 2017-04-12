6cf4cfa0e49f374cfb91e6dca8f35c005ab3cb33
samuelsp <samuelsp@media.mit.edu>
2017-04-12 18:40:42 -0400

    first commit

--- data_funcs.py
+++ data_funcs.py
@@ -51,9 +51,27 @@ class DataLoader:
                                                             self.wanted_labels, 'Test')
         if not suppress_output: print len(self.test_X), "rows in testing data"
     
+    def get_personalized_train_batch(self, batch_size, subject_num):
+
+        #print('subject is')
+        #print(subject_num)
+
+        personal_train_X, personal_train_Y = get_personalized_matrices_for_dataset(self.df, self.wanted_feats, 
+                                                            self.wanted_labels, 'Train', subject_num)
+        idx = np.random.choice(len(personal_train_X), size=batch_size)
+        #print('personal train X and Y are')
+        #print(personal_train_X)
+        #print(personal_train_Y)
+        return personal_train_X[idx], personal_train_Y[idx]
+
     def get_train_batch(self, batch_size):
         idx = np.random.choice(len(self.train_X), size=batch_size)
         return self.train_X[idx], self.train_Y[idx]
+
+    def get_personalized_val_data(self, subject_num):
+        personal_val_X, personal_val_Y = get_personalized_matrices_for_dataset(self.df, self.wanted_feats, 
+                                                            self.wanted_labels, 'Val', subject_num)
+        return personal_val_X, personal_val_Y
 
     def get_val_data(self):
         return self.val_X, self.val_Y
@@ -90,6 +108,21 @@ def normalize_fill_df(data_df, wanted_feats, wanted_labels, suppress_output=Fals
     data_df = data_df.sample(frac=1)
 
     return data_df
+
+def get_personalized_matrices_for_dataset(data_df, wanted_feats, wanted_labels, dataset, subject_num, single_output=False):
+    set_df = data_df[(data_df['dataset']==dataset) & (data_df['subject_num']==subject_num)]
+    
+    X = set_df[wanted_feats].astype(float).as_matrix()
+    
+    if single_output:
+        y = set_df[wanted_labels[0]].tolist()
+    else:
+        y = set_df[wanted_labels].as_matrix()
+    
+    X = convert_matrix_tf_format(X)
+    y = np.atleast_2d(np.asarray(y))
+
+    return X,y
 
 def get_matrices_for_dataset(data_df, wanted_feats, wanted_labels, dataset, single_output=False):
     set_df = data_df[data_df['dataset']==dataset]
